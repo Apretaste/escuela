5f573465461db4c683171bc49caec620a8504cde
kumahacker <kumahavana@gmail.com>
2017-02-27 00:27:52 -0500

    fix courses order

--- service.php
+++ service.php
@@ -72,7 +72,7 @@ class Escuela extends Service
         $l = count($currentcourses);
         for($i = 0; $i < $l - 1; $i++)
             for ($j = $i + 1; $j < $l; $j++)
-                if ($currentcourses[$i]->progress > $currentcourses[$j]->progress)
+                if ($currentcourses[$i]->progress < $currentcourses[$j]->progress)
                 {
                     $temp = $currentcourses[$i];
                     $currentcourses[$i] = $currentcourses[$j];
