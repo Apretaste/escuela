c335bc1d4cdecef11abab3077a6c864dda78a6ba
kumahacker <kumahavana@gmail.com>
2019-05-25 00:36:57 -0500

    update

--- service.php
+++ service.php
@@ -668,7 +668,8 @@ class Service {
 	private function getCourse($id, $email = '') {
 		$res = Connection::query("	SELECT *,
 				(SELECT name FROM _escuela_teacher WHERE _escuela_teacher.id = _escuela_course.teacher) AS teacher_name,
-				(SELECT title FROM _escuela_teacher WHERE _escuela_teacher.id = _escuela_course.teacher) AS teacher_title
+				(SELECT title FROM _escuela_teacher WHERE _escuela_teacher.id = _escuela_course.teacher) AS teacher_title,
+				(SELECT count(*) FROM _escuela_stars WHERE _escuela_stars.person_id = (SEKECT id FROM person WHERE person.email = '$email') AND _escuela_stars.course = _escuela_course.id) > 0 as rated
 			FROM _escuela_course
 			WHERE id= '$id'
 			AND active=1", true, 'latin1');
