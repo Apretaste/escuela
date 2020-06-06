990fec5d281f13a10825e06ceffa5c06f3f63f06
Ricardo <jimenezricardo98@gmail.com>
2020-06-06 12:24:21 -0500

    add pollyfill

--- scripts.js
+++ scripts.js
@@ -456,3 +456,55 @@ if (!String.prototype.padEnd) {
 		}
 	};
 }
+
+function _typeof(obj) {
+	if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+		_typeof = function _typeof(obj) {
+			return typeof obj;
+		};
+	} else {
+		_typeof = function _typeof(obj) {
+			return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+		};
+	}
+	return _typeof(obj);
+}
+
+if (!Object.keys) {
+	Object.keys = function () {
+		'use strict';
+
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+			hasDontEnumBug = !{
+				toString: null
+			}.propertyIsEnumerable('toString'),
+			dontEnums = ['toString', 'toLocaleString', 'valueOf', 'hasOwnProperty', 'isPrototypeOf', 'propertyIsEnumerable', 'constructor'],
+			dontEnumsLength = dontEnums.length;
+
+		return function (obj) {
+			if (_typeof(obj) !== 'object' && (typeof obj !== 'function' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object');
+			}
+
+			var result = [],
+				prop,
+				i;
+
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop);
+				}
+			}
+
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i]);
+					}
+				}
+			}
+
+			return result;
+		};
+	}();
+}
